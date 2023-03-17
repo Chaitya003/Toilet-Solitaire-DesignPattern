45ea147f570a4d243d905b16c46b2160ea63b603
Chaitya Sanghavi <chaityasanghavi33@gmail.com>
2023-03-16 22:35:15 -0700

    Cleaned the code and added some comments for a better understanding

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ I have run the file in Intelli J with its IDE run option:
 
 This is the link of the Taiga Kanban board: https://tree.taiga.io/project/chaitya33-ser515-toilet-solitaire-design-pattern/kanban
 
-The game.java file is using different objects to run the logic of the game. It contains all the logic of the rules defined and the player can play the game as much as he/she wants.
+For running the program in the CommandLine run the Game.java file.
+The Game.java file is using different objects to run the logic of the game. It contains all the logic of the rules defined and the player can play the game as much as he/she wants.
+
+For playing the game with GUI run SolitaireGUI.java file.
+The SolitaireGUI.java file is using swiing library to implement all the rules and logic of the Solitaire game.
--- SolitaireGUI.java
+++ SolitaireGUI.java
@@ -9,18 +9,14 @@ public class SolitaireGUI extends JFrame {
     private JLabel statusLabel;
     private ArrayList<Cards> deck;
     private ArrayList<String> cardDeck;
+    private int cnt=0, gameRule=0;
 
     public SolitaireGUI(ArrayList<String> cardDeck) {
         super("Card Game");
         this.cardDeck = cardDeck;
 
         // Initialize the deck of cards
-//        Deck deck = Deck.getObject();
-//        ArrayList<String> cardDeck = new ArrayList<>();
         ArrayList<String> hand = new ArrayList<>();
-//        ArrayList<String> new_hand = new ArrayList<>();
-//        cardDeck = deck.createDeck();
-//        cardDeck = deck.shuffleDeck(cardDeck);
 
         CardOperation cardoperation = new CardOperation();
         Command handCommand = new Hand(cardDeck, hand);
@@ -31,7 +27,6 @@ public class SolitaireGUI extends JFrame {
         Command removeCardDeck = new RemoveCard(cardDeck, numRemoveCardsDeck, false);
         cardoperation.setCommand(removeCardDeck);
         this.cardDeck = cardoperation.cardEvent();
-//        System.out.println();
 
         // Create the GUI components
         mainPanel = new JPanel();
@@ -52,7 +47,6 @@ public class SolitaireGUI extends JFrame {
         cardPanel.add(intermediateRule);
         cardPanel.add(houseRule);
 
-//        mainPanel.add(cardPanel);
         setLayout(new BorderLayout());
         add(cardPanel, BorderLayout.CENTER);
         add(statusLabel, BorderLayout.NORTH);
@@ -61,16 +55,12 @@ public class SolitaireGUI extends JFrame {
         ArrayList<String> finalHand = hand;
 
         basicRule.addActionListener(e -> {
+            gameRule = 1;
             // Deal a card and update the status label
             Cards card = new Cards();
             for(int i = 0; i< finalHand.size(); i++){
                 cardPanel.add(new JLabel(card.getImage(finalHand.get(i))));
             }
-
-//            basicPanel = new JPanel();
-//            Remove2Cards = new JButton("Remove Middle 2 Cards");
-//            Remove4Cards = new JButton("Remove All Four Cards");
-//            drawCard = new JButton("Draw A New Card");
 
             basicPanel.add(Remove2Cards);
             basicPanel.add(Remove4Cards);
@@ -88,6 +78,7 @@ public class SolitaireGUI extends JFrame {
 
 
         intermediateRule.addActionListener(e -> {
+            gameRule = 2;
             // Deal a card and update the status label
             Cards card = new Cards();
             for(int i = 0; i< finalHand.size(); i++){
@@ -95,10 +86,6 @@ public class SolitaireGUI extends JFrame {
             }
 
             intermediatePanel = new JPanel();
-//            Remove2Cards = new JButton("Remove Middle 2 Cards");
-//            Remove4Cards = new JButton("Remove All Four Cards");
-//            RemovePairCards = new JButton("Remove Pair of Cards");
-//            drawCard = new JButton("Draw A New Card");
 
             intermediatePanel.add(Remove2Cards);
             intermediatePanel.add(Remove4Cards);
@@ -116,6 +103,7 @@ public class SolitaireGUI extends JFrame {
         });
 
         houseRule.addActionListener(e -> {
+            gameRule = 3;
             // Deal a card and update the status label
             Cards card = new Cards();
             for(int i = 0; i< finalHand.size(); i++){
@@ -149,11 +137,12 @@ public class SolitaireGUI extends JFrame {
 
         /* Operations of Card Draw and Removal */
 
-
-//        ArrayList<String> finalCardDeck = cardDeck;
         ArrayList<String> finalHand1 = hand;
         Remove2Cards.addActionListener(e -> {
             int nextMove = 1;
+            if(this.cardDeck.isEmpty()){
+                cnt+=1;
+            }
             StrategyController strategy = new StrategyController(new BasicRule());
             ArrayList<String> new_hand = strategy.executeStrategy(this.cardDeck, finalHand1, nextMove);
 
@@ -171,11 +160,14 @@ public class SolitaireGUI extends JFrame {
                 cardPanel.add(new JLabel(card.getImage(new_hand.get(i))));
             }
 
-//            basicPanel = new JPanel();
-//            Remove2Cards = new JButton("Remove Middle 2 Cards");
-//            Remove4Cards = new JButton("Remove All Four Cards");
-//            drawCard = new JButton("Draw A New Card");
-//            basicPanel.getParent().remove(basicPanel);
+            CheckWin checkWin = new CheckWin();
+            ResultObserver resultObserver = new ResultObserver();
+            checkWin.attach(resultObserver);
+            checkWin.setState(this.cardDeck, new_hand, cnt);
+            boolean flag = checkWin.getState();
+            if (!flag) {
+                System.exit(0);
+            }
 
             basicPanel.add(Remove2Cards);
             basicPanel.add(Remove4Cards);
@@ -183,19 +175,17 @@ public class SolitaireGUI extends JFrame {
 
 
             cardPanel.add(basicPanel);
-//            mainPanel.add(cardPanel);
-            this.add(mainPanel);
+
             setLayout(new BorderLayout());
             add(cardPanel, BorderLayout.SOUTH);
-//            basicRule.getParent().remove(basicRule);
-//            intermediateRule.getParent().remove(intermediateRule);
-//            houseRule.getParent().remove(houseRule);
-
             pack();
         });
 
         Remove4Cards.addActionListener(e -> {
             int nextMove = 2;
+            if(this.cardDeck.isEmpty()){
+                cnt+=1;
+            }
             StrategyController strategy = new StrategyController(new BasicRule());
             ArrayList<String> new_hand = strategy.executeStrategy(this.cardDeck, finalHand1, nextMove);
             // Deal a card and update the status label
@@ -212,34 +202,63 @@ public class SolitaireGUI extends JFrame {
             for(int i = new_hand.size()-4; i< new_hand.size(); i++){
                 cardPanel.add(new JLabel(card.getImage(new_hand.get(i))));
             }
-
-//            basicPanel = new JPanel();
-//            Remove2Cards = new JButton("Remove Middle 2 Cards");
-//            Remove4Cards = new JButton("Remove All Four Cards");
-//            drawCard = new JButton("Draw A New Card");
-//            basicPanel.getParent().remove(basicPanel);
+            CheckWin checkWin = new CheckWin();
+            ResultObserver resultObserver = new ResultObserver();
+            checkWin.attach(resultObserver);
+            checkWin.setState(this.cardDeck, new_hand, cnt);
+            boolean flag = checkWin.getState();
+            if (!flag) {
+                System.exit(0);
+            }
 
             basicPanel.add(Remove2Cards);
             basicPanel.add(Remove4Cards);
             basicPanel.add(drawCard);
 
             cardPanel.add(basicPanel);
-//            mainPanel.add(cardPanel);
-//            this.add(mainPanel);
             setLayout(new BorderLayout());
             add(cardPanel, BorderLayout.SOUTH);
-//            basicRule.getParent().remove(basicRule);
-//            intermediateRule.getParent().remove(intermediateRule);
-//            houseRule.getParent().remove(houseRule);
-
             pack();
         });
 
         drawCard.addActionListener(e -> {
             int nextMove = 9;
-            StrategyController strategy = new StrategyController(new BasicRule());
-            ArrayList<String> new_hand = strategy.executeStrategy(this.cardDeck, finalHand1, nextMove);
-            // Deal a card and update the status label
+            ArrayList<String> new_hand = new ArrayList<String>();
+            if(this.cardDeck.isEmpty()){
+                cnt+=1;
+            }
+            if(gameRule==1) {
+                StrategyController strategy = new StrategyController(new BasicRule());
+                new_hand = strategy.executeStrategy(this.cardDeck, finalHand1, nextMove);
+                basicPanel.add(Remove2Cards);
+                basicPanel.add(Remove4Cards);
+                basicPanel.add(drawCard);
+
+                cardPanel.add(basicPanel);
+            } else if (gameRule==2) {
+                StrategyController strategy = new StrategyController(new IntermediateRule());
+                new_hand = strategy.executeStrategy(this.cardDeck, finalHand1, nextMove);
+                intermediatePanel.add(Remove2Cards);
+                intermediatePanel.add(Remove4Cards);
+                intermediatePanel.add(RemovePairCards);
+                intermediatePanel.add(drawCard);
+
+
+                cardPanel.add(intermediatePanel);
+            }
+            else{
+                StrategyController strategy = new StrategyController(new HouseRule());
+                new_hand = strategy.executeStrategy(this.cardDeck, finalHand1, nextMove);
+                housePanel.add(Remove2Cards);
+                housePanel.add(Remove4Cards);
+                housePanel.add(RemovePairCards);
+                housePanel.add(RemoveLast3);
+                housePanel.add(RemoveFirst3);
+                housePanel.add(drawCard);
+
+
+                cardPanel.add(housePanel);
+            }
             int numRemoveCardsDeckRule[] = {0, 0};
             Command removeCardDeckRule = new RemoveCard(this.cardDeck, numRemoveCardsDeckRule, false);
             cardoperation.setCommand(removeCardDeckRule);
@@ -247,11 +266,9 @@ public class SolitaireGUI extends JFrame {
 
             cardPanel.getParent().remove(cardPanel);
 
-//            System.out.println();
             for(int i=0; i<4; i++){
                 cardPanel.remove(0);
             }
-//            cardPanel.remove(0);
 
             Cards card = new Cards();
             for(int i = new_hand.size()-4; i< new_hand.size(); i++){
@@ -259,28 +276,153 @@ public class SolitaireGUI extends JFrame {
                 cardPanel.add(new JLabel(card.getImage(new_hand.get(i))));
             }
 
+            CheckWin checkWin = new CheckWin();
+            ResultObserver resultObserver = new ResultObserver();
+            checkWin.attach(resultObserver);
+            checkWin.setState(this.cardDeck, new_hand, cnt);
+            boolean flag = checkWin.getState();
+            if (!flag) {
+                System.exit(0);
+            }
 
-//            basicPanel = new JPanel();
-//            Remove2Cards = new JButton("Remove Middle 2 Cards");
-//            Remove4Cards = new JButton("Remove All Four Cards");
-//            drawCard = new JButton("Draw A New Card");
-//            basicPanel.getParent().remove(basicPanel);
-
-            basicPanel.add(Remove2Cards);
-            basicPanel.add(Remove4Cards);
-            basicPanel.add(drawCard);
-
-            cardPanel.add(basicPanel);
-//            mainPanel.add(cardPanel);
-//            this.add(mainPanel);
-//            setLayout(new BorderLayout());
             add(cardPanel, BorderLayout.SOUTH);
-//            basicRule.getParent().remove(basicRule);
-//            intermediateRule.getParent().remove(intermediateRule);
-//            houseRule.getParent().remove(houseRule);
-
             pack();
         });
+
+        RemovePairCards.addActionListener(e -> {
+            int nextMove = 3;
+            if(this.cardDeck.isEmpty()){
+                cnt+=1;
+            }
+            StrategyController strategy = new StrategyController(new IntermediateRule());
+            ArrayList<String> new_hand = strategy.executeStrategy(this.cardDeck, finalHand1, nextMove);
+
+            int numRemoveCardsDeckRule[] = {0, 1};
+            Command removeCardDeckRule = new RemoveCard(this.cardDeck, numRemoveCardsDeckRule, false);
+            cardoperation.setCommand(removeCardDeckRule);
+            this.cardDeck = cardoperation.cardEvent();
+            cardPanel.getParent().remove(cardPanel);
+            for(int i=0; i<4; i++){
+                cardPanel.remove(0);
+            }
+            // Deal a card and update the status label
+            Cards card = new Cards();
+            for(int i = new_hand.size()-4; i< new_hand.size(); i++){
+                cardPanel.add(new JLabel(card.getImage(new_hand.get(i))));
+            }
+
+            CheckWin checkWin = new CheckWin();
+            ResultObserver resultObserver = new ResultObserver();
+            checkWin.attach(resultObserver);
+            checkWin.setState(this.cardDeck, new_hand, cnt);
+            boolean flag = checkWin.getState();
+            if (!flag) {
+                System.exit(0);
+            }
+
+            intermediatePanel.add(Remove2Cards);
+            intermediatePanel.add(Remove4Cards);
+            intermediatePanel.add(RemovePairCards);
+            intermediatePanel.add(drawCard);
+
+
+            cardPanel.add(intermediatePanel);
+            setLayout(new BorderLayout());
+            add(cardPanel, BorderLayout.SOUTH);
+            pack();
+        });
+
+        RemoveLast3.addActionListener(e -> {
+            int nextMove = 4;
+            if(this.cardDeck.isEmpty()){
+                cnt+=1;
+            }
+            StrategyController strategy = new StrategyController(new HouseRule());
+            ArrayList<String> new_hand = strategy.executeStrategy(this.cardDeck, finalHand1, nextMove);
+
+            int numRemoveCardsDeckRule[] = {0, 2};
+            Command removeCardDeckRule = new RemoveCard(this.cardDeck, numRemoveCardsDeckRule, false);
+            cardoperation.setCommand(removeCardDeckRule);
+            this.cardDeck = cardoperation.cardEvent();
+            cardPanel.getParent().remove(cardPanel);
+            for(int i=0; i<4; i++){
+                cardPanel.remove(0);
+            }
+            // Deal a card and update the status label
+            Cards card = new Cards();
+            for(int i = new_hand.size()-4; i< new_hand.size(); i++){
+                cardPanel.add(new JLabel(card.getImage(new_hand.get(i))));
+            }
+
+            CheckWin checkWin = new CheckWin();
+            ResultObserver resultObserver = new ResultObserver();
+            checkWin.attach(resultObserver);
+            checkWin.setState(this.cardDeck, new_hand, cnt);
+            boolean flag = checkWin.getState();
+            if (!flag) {
+                System.exit(0);
+            }
+
+            housePanel.add(Remove2Cards);
+            housePanel.add(Remove4Cards);
+            housePanel.add(RemovePairCards);
+            housePanel.add(RemoveLast3);
+            housePanel.add(RemoveFirst3);
+            housePanel.add(drawCard);
+
+
+            cardPanel.add(housePanel);
+            setLayout(new BorderLayout());
+            add(cardPanel, BorderLayout.SOUTH);
+            pack();
+        });
+
+        RemoveFirst3.addActionListener(e -> {
+            int nextMove = 5;
+            if(this.cardDeck.isEmpty()){
+                cnt+=1;
+            }
+            StrategyController strategy = new StrategyController(new HouseRule());
+            ArrayList<String> new_hand = strategy.executeStrategy(this.cardDeck, finalHand1, nextMove);
+
+            int numRemoveCardsDeckRule[] = {0, 2};
+            Command removeCardDeckRule = new RemoveCard(this.cardDeck, numRemoveCardsDeckRule, false);
+            cardoperation.setCommand(removeCardDeckRule);
+            this.cardDeck = cardoperation.cardEvent();
+            cardPanel.getParent().remove(cardPanel);
+            for(int i=0; i<4; i++){
+                cardPanel.remove(0);
+            }
+            // Deal a card and update the status label
+            Cards card = new Cards();
+            for(int i = new_hand.size()-4; i< new_hand.size(); i++){
+                cardPanel.add(new JLabel(card.getImage(new_hand.get(i))));
+            }
+
+            CheckWin checkWin = new CheckWin();
+            ResultObserver resultObserver = new ResultObserver();
+            checkWin.attach(resultObserver);
+            checkWin.setState(this.cardDeck, new_hand, cnt);
+            boolean flag = checkWin.getState();
+            if (!flag) {
+                System.exit(0);
+            }
+
+            housePanel.add(Remove2Cards);
+            housePanel.add(Remove4Cards);
+            housePanel.add(RemovePairCards);
+            housePanel.add(RemoveLast3);
+            housePanel.add(RemoveFirst3);
+            housePanel.add(drawCard);
+
+
+            cardPanel.add(housePanel);
+            setLayout(new BorderLayout());
+            add(cardPanel, BorderLayout.SOUTH);
+            pack();
+        });
+
+
 
 
         // Set some default settings for the main window
